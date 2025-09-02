ff0efe4492fede41ccc6348552fd4b05e6dffe80
Cooper Frank <coopfrank04@gmail.com>
2025-09-02 16:27:34 -0400

    Matched expected output in test file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,13 @@
 #include "math_utils.h"
 #include "advanced_math.h"
 
-using std::cin, std::cout, std::endl;
+using std::cout, std::endl;
 
 int main() {
-    cout << "Enter your first number: ";
+    int a = 3;
+    int b = 4;
 
-    int n1;
-    cin >> n1;
-
-    cout << "Enter your second number: ";
-
-    int n2;
-    cin >> n2;
-
-    cout << n1 << " + " << n2 << " is " << MathUtils::add(n1, n2) << "." << endl;
-
-    cout << n1 << " * " << n2 << " is " << MathUtils::multiply(n1, n2) << "." << endl;
-
-    cout << n1 << "^2 is " << AdvancedMath::square(n1) << "." << endl;
-    cout << n2 << "^2 is " << AdvancedMath::square(n2) << "." << endl;
+    cout << "a + b = " << MathUtils::add(a, b) << endl;
+    cout << "a * b = " << MathUtils::multiply(a, b) << endl;
+    cout << "square(a) = " << AdvancedMath::square(a) << endl;
 }
